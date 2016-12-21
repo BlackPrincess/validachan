440f0ed4d358d178c1f2dbff6839f834cc9b8a78
Kurokawa <black.princess.w@gmail.com>
2016-12-21 14:39:28 +0900

    test

---
+++ test/buildin/required_validator_test.js
@@ -0,0 +1,13 @@
+import assert from 'power-assert'
+import {RequiredValidator} from '../../src/buildin/required_validator.js'
+
+describe('RequiredValidator', () => {
+  it('exports world', () => {
+    const noop = 'noop'
+    const testTarget = {foo: 'foo value', bar: '', baz: null}
+    assert(RequiredValidator.validateEach(testTarget, 'foo', noop) === null)
+    assert(RequiredValidator.validateEach(testTarget, 'bar', noop) !== null)
+    assert(RequiredValidator.validateEach(testTarget, 'baz', noop) !== null)
+    assert(RequiredValidator.validateEach(testTarget, 'undef', noop) !== null)
+  })
+})
